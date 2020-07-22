1538749d74a5bd0742d7d6fa07d2bc76ce1c4938
David Zhao <david@davidzhao.com>
2020-07-22 00:36:17 -0700

    version bump to v0.4.0

--- Formula/konstellation.rb
+++ Formula/konstellation.rb
@@ -2,9 +2,9 @@ class Konstellation < Formula
   desc "Manages Apps on Kubernetes"
   homepage "https://github.com/k11n/konstellation"
   url "https://github.com/k11n/konstellation.git",
-    :tag => "v0.3.3"
+    :tag => "v0.4.0"
   head "https://github.com/k11n/konstellation.git"
-  version "0.3.3"
+  version "0.4.0"
 
   depends_on "go" => :build
   depends_on "kubernetes-cli"
