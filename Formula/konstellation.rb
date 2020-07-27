c723acc2735f240d52744599d8e78afc748ba5ea
David Zhao <david@davidzhao.com>
2020-07-26 22:56:28 -0700

    version 0.4.1, fixed broken builds

--- Formula/konstellation.rb
+++ Formula/konstellation.rb
@@ -2,26 +2,27 @@ class Konstellation < Formula
   desc "Manages Apps on Kubernetes"
   homepage "https://github.com/k11n/konstellation"
   url "https://github.com/k11n/konstellation.git",
-    :tag => "v0.4.0"
+    :tag => "v0.4.1"
   head "https://github.com/k11n/konstellation.git"
-  version "0.4.0"
+  version "0.4.1"
 
   depends_on "go" => :build
   depends_on "kubernetes-cli"
   depends_on "terraform"
 
   def install
-    kon_dir = "cmd/kon"
     gopath = buildpath/"gopath"
     mkdir gopath
     ENV["GOPATH"] = gopath
     bin_dir = gopath/"bin"
     mkdir bin_dir
     ENV["PATH"] = "#{ENV["PATH"]}:#{bin_dir}"
+    kon_dir = "cmd/kon"
 
     cd kon_dir do
       system "go", "get", "github.com/GeertJohan/go.rice/rice"
-      system "./build.sh"
+      system "rice", "embed-go", "--import-path", "github.com/k11n/konstellation/cmd/kon/utils"
+      system "go", "build", "-o", "kon"
       bin.install "kon"
     end
   end
