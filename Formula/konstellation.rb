8a2571332917e0539ac63eacaf258eee30028c37
David Zhao <david@davidzhao.com>
2021-03-30 17:47:42 -0700

    v0.5.7

--- Formula/konstellation.rb
+++ Formula/konstellation.rb
@@ -2,9 +2,9 @@ class Konstellation < Formula
   desc "Manages Apps on Kubernetes"
   homepage "https://github.com/k11n/konstellation"
   url "https://github.com/k11n/konstellation.git",
-    :tag => "v0.5.5"
+    :tag => "v0.5.7"
   head "https://github.com/k11n/konstellation.git"
-  version "0.5.5"
+  version "0.5.7"
 
   depends_on "go" => :build
   depends_on "kubernetes-cli"
